cfcd5039a31ae6f817bca595616bd070e21f4516
Nick Daniels <nick@lgcstp.co>
2023-07-16 14:59:59 -0400

    Bump nabunetwork.com version

--- scripts/napa-tools.psm1
+++ scripts/napa-tools.psm1
@@ -1,4 +1,17 @@
 
+function EnsureFolder([string]$Path) {
+    if (-not (Test-Path $Path)) {
+        New-Item -Path $Path -ItemType Directory;
+    }
+}
+
+function ArchiveFolder([string]$Path,[string]$PackagesFolder) {
+    $archiveFolder = Join-Path $PackagesFolder 'archive';
+    EnsureFolder $archiveFolder;
+    $name = [System.IO.Path]::GetFileName($Path);
+    $filename = "${name}-$((Get-Date).ToString("yyyy-MM-dd-HH-mm-ss")).zip";
+    Compress-Archive -Path $destination -DestinationPath "$(Join-Path $archiveFolder $filename)";
+}
 
 function Install-NAPAPackage {
     [cmdletbinding()]
@@ -6,10 +19,13 @@ function Install-NAPAPackage {
         [string]$Package,
         [string]$PackagesFolder
     )
+    $ErrorActionPreference = 'Stop';
+    
     $name = [System.IO.Path]::GetFileNameWithoutExtension($Package);
     $destination = (Join-Path $PackagesFolder $name);
 
-    if ((Test-Path $destination)){
+    if ((Test-Path $destination)){  
+        ArchiveFolder $destination $PackagesFolder
         Remove-Item -Path $destination -Force -Recurse;
     } 
 
